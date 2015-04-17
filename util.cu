199de0fa977664b58a38292f8cd2deb0c1b2517a
Little Sheep <eaa3@cin.ufpe.br>
2015-04-17 17:06:55 -0300

    added usage instructions

--- util.cu
+++ util.cu
@@ -1,9 +1,10 @@
 #include "util.cuh"
 #include "nbody.cuh"
 
+#include <string>
 
 extern Body bodies[N_SIZE];
-float cx=-200,cy=-200,cz=-500;
+float prevX = WINDOW_W/2, prevY = WINDOW_H/2;
 
 void timerFunc(int value)
 {
@@ -40,14 +41,14 @@ void keyboardFunc(unsigned char key, int x, int y) {
     if (key == 27) 
         exit(0);
 
-    float vel = 10;
+    float vel = 5.0;
     float rightX, rightY, rightZ;
-    printf("forwardX: %f forwardY: %f forwardZ: %f\n",camera.forwardX,camera.forwardY,camera.forwardZ);
-    printf("upX: %f upY: %f upZ: %f\n", camera.upX,camera.upY, camera.upZ);
+    //printf("forwardX: %f forwardY: %f forwardZ: %f\n",camera.forwardX,camera.forwardY,camera.forwardZ);
+    //printf("upX: %f upY: %f upZ: %f\n", camera.upX,camera.upY, camera.upZ);
     cross(camera.forwardX,camera.forwardY,camera.forwardZ, camera.upX,camera.upY, camera.upZ, rightX, rightY, rightZ);
     float sizeRight = sqrtf(rightX*rightX + rightY*rightY + rightZ*rightZ);
     rightX /= sizeRight; rightY /= sizeRight; rightZ /= sizeRight;
-    printf("rightX: %f rightY: %f rightZ: %f\n",rightX, rightY, rightZ);
+    //printf("rightX: %f rightY: %f rightZ: %f\n",rightX, rightY, rightZ);
     if( key == 'w' )
     {
         camera.camX += camera.forwardX*vel;
@@ -73,7 +74,7 @@ void keyboardFunc(unsigned char key, int x, int y) {
         camera.camY += rightY*vel;
         camera.camZ += rightZ*vel;
     }
-
+     printf("camX: %f camY: %f camZ: %f\n",camera.camX,camera.camY,camera.camY);
 
 
 }
@@ -81,7 +82,9 @@ void keyboardFunc(unsigned char key, int x, int y) {
 void mouseCallback(int x, int y){
 
 
-        camera.phi = M_PI+ (0.5 - (float(x)/WINDOW_W))*M_PI;
+        //camera.phi += (0.5 - (float(x)/WINDOW_W))*M_PI*0.015;
+        //camera.theta += (0.5 - (float(y)/WINDOW_H))*M_PI*0.015; 
+        camera.phi = M_PI + (0.5 - (float(x)/WINDOW_W))*M_PI;
         camera.theta = (0.5 - (float(y)/WINDOW_H))*M_PI; 
 
 
@@ -171,11 +174,32 @@ void DrawCircle(float cx, float cy, float r, int num_segments) {
     glEnd();
 }
 
+void drawText(std::string text, float x, float y){
+    glMatrixMode(GL_MODELVIEW);
+    glPushMatrix();
+
+    //glLoadIdentity();
+
+    glColor3f(1.0f, 0.0f, 0.0f);//needs to be called before RasterPos
+    glRasterPos2f(x, y);
+    //glScalef(10,10,10);
+    
+    void * font = GLUT_BITMAP_TIMES_ROMAN_24;
+
+    for (std::string::iterator i = text.begin(); i != text.end(); ++i)
+    {
+        char c = *i;
+        //this does nothing, color is fixed for Bitmaps when calling glRasterPos
+        //glColor3f(1.0, 0.0, 1.0); 
+        glutBitmapCharacter(font, c);
+    }
+    glPopMatrix();
+}
 
 void draw2(){
     glClearColor(0.7f,0.7f,0.7f,0.7f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glColor3f(0.5f, 0.0f, 1.0f);
+    
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
@@ -184,7 +208,11 @@ void draw2(){
               camera.upX,camera.upY,camera.upZ); //Camera up direction
 
     runKernelNBodySimulation();
+    drawText("INFO", 50,40);
+    drawText("Use keys w, a, s, d to move", 50,50);
+    drawText("Hold mouse+left button to look around", 50,60);
 
+    glColor3f(0.5f, 0.0f, 1.0f);
     for(int i = 0; i < N_SIZE; i ++){
         if(bodies[i].alpha>0)
         {
